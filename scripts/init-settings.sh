4e7f939eddc135975d04395ca5bc390e9b06d287
SuLingGG <1215009714@qq.com>
2021-02-19 17:24:07 +0800

    Add build date to index page

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -12,4 +12,8 @@ uci set luci.main.mediaurlbase='/luci-static/argon'
 # Disable opkg signature check
 sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
 
+# Add build date to index page
+export orig_version="$(cat "/etc/openwrt_release" | grep DISTRIB_DESCRIPTION | awk -F "'" '{print $2}')"
+sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" /etc/openwrt_release
+
 exit 0

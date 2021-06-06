097540d8da7957118499d8feb772f03f89d982c5
SuLingGG <1215009714@QQ.COM>
2021-06-06 19:27:13 +0800

    USB_LAN78XX CM4 device support optimization

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -19,10 +19,4 @@ sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" /etc/openwrt_
 # Add execute permission for ipv6-helper
 chmod +x /bin/ipv6-helper
 
-# Add support for USB_LAN78XX Pi Compute Module 4 carrier boards
-cd /lib/firmware/brcm
-if [ `ls -l|grep '4-compute-module.txt'|wc -l` -eq 0 ]; then
-    cp brcmfmac43455-sdio.raspberrypi,4-model-b.txt brcmfmac43455-sdio.raspberrypi,4-compute-module.txt
-fi
-
 exit 0
